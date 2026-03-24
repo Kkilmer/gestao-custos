1f569d6155f92bbcac91f301863d0f5c349d6ae1
kkilmer <kp827767@gmail.com>
2026-03-24 07:55:26 -0300

    Manipulação no BD

---
+++ src/main/java/br/com/javadevweek/gestao_custos/performance/GestaoDeDespesaSeeder.java
@@ -0,0 +1,13 @@
+package br.com.javadevweek.gestao_custos.performance;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GestaoDeDespesaSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
